883e32fdb25458ac0e2a883fc7498b15a2345d18
soespa <soeren.sparmann@uni-paderborn.de>
2023-09-21 13:57:20 +0200

    Analyse in andere Datei auslagern

--- dashboard.py
+++ dashboard.py
@@ -1,12 +1,16 @@
+import numpy as np
 import streamlit as st
 import pandas as pd
 from bertopic import BERTopic
 import os
 import plotly.graph_objects as go
+from umap import UMAP
+
+from util import *
 
 st.set_page_config(
     page_title="Topic Model",
-    layout='wide',
+    #layout='wide',
     initial_sidebar_state='collapsed'
 )
 
@@ -14,6 +18,9 @@ st.markdown('# Topic Model für die Kernlehrpläne Informatik Sekundarstufe I & 
 
 documents = pd.read_json(os.path.join('data', 'documents.json'))
 sentences = pd.read_json(os.path.join('data', 'sentences.json'))
+
+docs = sentences['sentence']
+
 
 st.markdown('## Datengrundlage')
 
@@ -34,12 +41,18 @@ with cols[2]:
 st.markdown('## Themen')
 
 topic_model = BERTopic.load(os.path.join('model', 'topic_model_merged.pkl'))
+#topic_model = BERTopic.load(os.path.join('model', 'topic_model_merged'))
 
 df_topics = topic_model.get_topic_info().set_index('Topic')[['CustomName', 'Count', 'Representation']]
 
 other_label = 'Sonstiges'
 
+other = df_topics.loc[-1]
+other.loc['CustomName'] = other_label
+
 df_topics = df_topics.drop(-1)
+
+df_topics.loc[-1] = other
 
 df_topics = df_topics.rename({
     'CustomName': 'Label',
@@ -59,8 +72,11 @@ st.download_button(
 )
 
 probabilities = topic_model.probabilities_
+topics = topic_model.get_topic_info()
+labels = topics['CustomName'].tolist()[1:]
+prop = pd.DataFrame(probabilities, columns=labels)
+prop[OTHER_LABEL] = 1 - prop.sum(axis=1)
 
-prop = pd.DataFrame(probabilities, columns=label)
 
 
 st.markdown('## Anteil der Themen insgesamt')
@@ -81,62 +97,21 @@ df = sentences.merge(documents, left_on='document', right_index=True, how='left'
 
 st.markdown('## Schwerpunkt nach Stufe')
 
-df_stufe = prop.groupby(df['stufe']).mean().T
-
-df_stufe['diff'] = (1 - (df_stufe['Sekundarstufe I'] / df_stufe['Sekundarstufe II'])) * 100
-
-df_stufe = df_stufe.sort_values('diff')
-
-
-fig = go.Figure(
-    data=[
-        go.Bar(
-            name='Sekundarstufe II',
-            y=df_stufe.index,
-            x=df_stufe['Sekundarstufe II'],
-            orientation='h'
-        ),
-        go.Bar(
-            name='Sekundarstufe I',
-            y=df_stufe.index,
-            x=df_stufe['Sekundarstufe I'],
-            orientation='h'
-        )
-    ],
-    layout=go.Layout(
-        height=800,
-    )
-)
-
-
-for index, row in df_stufe.iterrows():
-    value = row['diff']
-    if value > 0:
-        color = 'rgb(80, 180, 80)'
-        text = f"+{row['diff'].round(1)}%"
-    elif value < 0:
-        color = 'rgb(180, 80, 80)'
-        text = f"{row['diff'].round(1)}%"
-    else:
-        color = 'rgb(220, 220, 220)'
-        text = f"{row['diff'].round(1)}%"
-
-    fig.add_annotation(
-        dict(
-            xref='x',
-            yref='y',
-            x=row[['Sekundarstufe I', 'Sekundarstufe II']].max(),
-            y=index,
-            xanchor='left',
-            text=text,
-            font=dict(
-                family='Arial',
-                size=14,
-                color=color
-            ),
-            showarrow=False
-        )
-    )
+fig = plot_stufe(topic_model, df)
 
 st.plotly_chart(fig, use_container_width=True)
 
+st.markdown('Themenschwerpunkt nach Bundesland')
+
+fig = plot_states(topic_model, df)
+
+st.plotly_chart(fig, use_container_width=True)
+
+
+fig = topic_model.visualize_documents(docs, custom_labels=True, hide_annotations=True)
+
+fig.update_layout(
+    template="plotly"
+)
+
+st.plotly_chart(fig, use_container_width=True)
