9d9bae06824378c72cf5d834345efb491e067838
soespa <soeren.sparmann@uni-paderborn.de>
2023-09-21 14:49:21 +0200

    Change calculation for levels

--- dashboard.py
+++ dashboard.py
@@ -14,53 +14,27 @@ st.set_page_config(
     initial_sidebar_state='collapsed'
 )
 
+analysis = CurriculaAnalysis()
+
 st.markdown('# Topic Model für die Kernlehrpläne Informatik Sekundarstufe I & II')
 
-documents = pd.read_json(os.path.join('data', 'documents.json'))
-sentences = pd.read_json(os.path.join('data', 'sentences.json'))
-
-docs = sentences['sentence']
-
-
 st.markdown('## Datengrundlage')
-
-n_states = len(documents['bundesland'].unique())
-n_curricula = len(documents.groupby(['bundesland', 'stufe']))
 
 cols = st.columns(3)
 
 with cols[0]:
-    st.metric(label='Bundesländer', value=n_states)
+    st.metric(label='Bundesländer', value=analysis.n_states)
 
 with cols[1]:
-    st.metric(label='Kernlehrpläne', value=n_curricula)
+    st.metric(label='Kernlehrpläne', value=analysis.n_curricula)
 
 with cols[2]:
-    st.metric(label='Sätze', value=len(sentences))
+    st.metric(label='Sätze', value=analysis.n_sentences)
 
 st.markdown('## Themen')
 
-topic_model = BERTopic.load(os.path.join('model', 'topic_model_merged.pkl'))
-#topic_model = BERTopic.load(os.path.join('model', 'topic_model_merged'))
 
-df_topics = topic_model.get_topic_info().set_index('Topic')[['CustomName', 'Count', 'Representation']]
-
-other_label = 'Sonstiges'
-
-other = df_topics.loc[-1]
-other.loc['CustomName'] = other_label
-
-df_topics = df_topics.drop(-1)
-
-df_topics.loc[-1] = other
-
-df_topics = df_topics.rename({
-    'CustomName': 'Label',
-    'Representation': 'Schlüsselwörter',
-    'Count': 'Anzahl Sätze'
-}, axis=1)
-
-label = df_topics['Label']
+df_topics = analysis.df_topics
 
 st.dataframe(df_topics, use_container_width=True)
 
@@ -71,33 +45,18 @@ st.download_button(
     mime='application/json'
 )
 
-probabilities = topic_model.probabilities_
-topics = topic_model.get_topic_info()
-labels = topics['CustomName'].tolist()[1:]
-prop = pd.DataFrame(probabilities, columns=labels)
-prop[OTHER_LABEL] = 1 - prop.sum(axis=1)
-
-
-
 st.markdown('## Anteil der Themen insgesamt')
 
-fig = go.Figure(
-    data=[
-        go.Pie(
-            labels=label,
-            values=prop.mean()
-        )
-    ],
-)
+fig = analysis.get_total_topic_dist()
 
 st.plotly_chart(fig, use_container_width=True)
 
 
-df = sentences.merge(documents, left_on='document', right_index=True, how='left')
+
 
 st.markdown('## Schwerpunkt nach Stufe')
 
-fig = plot_stufe(topic_model, df)
+fig = analysis.plot_level()
 
 st.plotly_chart(fig, use_container_width=True)
 
--- util.py
+++ util.py
@@ -11,7 +11,7 @@ from umap import UMAP
 
 OTHER_LABEL = 'Sonstiges'
 
-complete_states = [
+COMPLETE_STATES = [
     'Baden-Württemberg',
     'Bayern',
     'Berlin',
@@ -50,6 +50,7 @@ custom_names = {
     19: "Suchmaschinen"
 }
 
+
 class CurriculaAnalysis():
 
     def __init__(self):
@@ -57,6 +58,63 @@ class CurriculaAnalysis():
         self.sentences = pd.read_json(os.path.join('data', 'sentences.json'))
 
         self.docs = self.sentences['sentence']
+
+        self.topic_model = BERTopic.load(os.path.join('model', 'topic_model_merged.pkl'))
+
+        self.df = self.sentences.merge(self.documents, left_on='document', right_index=True, how='left')
+
+    @property
+    def df_topics(self):
+        """
+        :return:
+        DataFrame for downloading
+        """
+        df_topics = self.topic_model.get_topic_info().set_index('Topic')[['CustomName', 'Count', 'Representation']]
+
+        df_topics.index.name = None
+
+        other = df_topics.loc[-1]
+        other.loc['CustomName'] = OTHER_LABEL
+
+        df_topics = df_topics.drop(-1)
+
+        df_topics.loc[-1] = other
+
+        df_topics = df_topics.rename({
+            'CustomName': 'Thema',
+            'Representation': 'Schlüsselwörter',
+            'Count': 'Anzahl'
+        }, axis=1)
+
+        return df_topics
+
+    @property
+    def df_props(self):
+        probabilities = self.topic_model.probabilities_
+        labels = self.topic_model.get_topic_info()['CustomName'].tolist()[1:]
+        df_props = pd.DataFrame(probabilities, columns=labels)
+        df_props[OTHER_LABEL] = 1 - df_props.sum(axis=1)
+
+        df_props = pd.concat([self.df[['bundesland', 'stufe']], df_props], axis=1)
+
+        return df_props
+
+    def get_total_topic_dist(self):
+
+        # Calculate mean for each curriculum
+        prop = self.df_props.groupby(['bundesland', 'stufe']).mean()
+
+        fig = go.Figure(
+            data=[
+                go.Pie(
+                    labels=prop.columns,
+                    values=prop.mean(),
+                    hole=0.3
+                )
+            ],
+        )
+
+        return fig
 
     @property
     def n_states(self):
@@ -66,77 +124,82 @@ class CurriculaAnalysis():
     def n_curricula(self):
         return len(self.documents.groupby(['bundesland', 'stufe']))
 
-def plot_level(topic_model: BERTopic, df: pd.DataFrame):
-    probabilities = topic_model.probabilities_
+    @property
+    def n_sentences(self):
+        return len(self.sentences)
 
-    topics = topic_model.get_topic_info()
 
-    labels = topics['CustomName'].tolist()[1:]
+    def plot_level(self):
+        df_props = self.df_props
 
-    prop = pd.DataFrame(probabilities, columns=labels)
+        df_props = df_props[df_props['bundesland'].isin(COMPLETE_STATES)]
 
-    prop[OTHER_LABEL] = 1 - prop.sum(axis=1)
+        df_curricula = df_props.groupby(['bundesland', 'stufe'], as_index=False).mean()
 
-    df_stufe = prop.groupby(df['stufe']).mean().T
+        st.write(df_curricula)
 
-    df_stufe['diff'] = (1 - (df_stufe['Sekundarstufe I'] / df_stufe['Sekundarstufe II'])) * 100
 
-    df_stufe = df_stufe.sort_values('diff')
+        #df_level = df_curricula.groupby('bundesland', as_index=False).apply(lambda g: g.set_index('stufe').unstack())
 
-    fig = go.Figure(
-        data=[
-            go.Bar(
-                name='Sekundarstufe II',
-                y=df_stufe.index,
-                x=df_stufe['Sekundarstufe II'],
-                orientation='h'
-            ),
-            go.Bar(
-                name='Sekundarstufe I',
-                y=df_stufe.index,
-                x=df_stufe['Sekundarstufe I'],
-                orientation='h'
-            )
-        ],
-        layout=go.Layout(
-            height=800,
-        )
-    )
+        st.write(df_level)
 
-    for index, row in df_stufe.iterrows():
-        value = row['diff']
-        if value > 0:
-            color = 'rgb(80, 180, 80)'
-            text = f"+{row['diff'].round(1)}%"
-        elif value < 0:
-            color = 'rgb(180, 80, 80)'
-            text = f"{row['diff'].round(1)}%"
-        else:
-            color = 'rgb(220, 220, 220)'
-            text = f"{row['diff'].round(1)}%"
+        df_level['diff'] = (1 - (df_level['Sekundarstufe I'] / df_level['Sekundarstufe II'])) * 100
 
-        fig.add_annotation(
-            dict(
-                xref='x',
-                yref='y',
-                x=row[['Sekundarstufe I', 'Sekundarstufe II']].max(),
-                y=index,
-                xanchor='left',
-                text=text,
-                font=dict(
-                    family='Arial',
-                    size=14,
-                    color=color
+        df_level = df_level.sort_values('diff')
+
+        fig = go.Figure(
+            data=[
+                go.Bar(
+                    name='Sekundarstufe II',
+                    y=df_level.index,
+                    x=df_level['Sekundarstufe II'],
+                    orientation='h'
                 ),
-                showarrow=False
+                go.Bar(
+                    name='Sekundarstufe I',
+                    y=df_level.index,
+                    x=df_level['Sekundarstufe I'],
+                    orientation='h'
+                )
+            ],
+            layout=go.Layout(
+                height=800,
             )
         )
+
+        for index, row in df_level.iterrows():
+            value = row['diff']
+            if value > 0:
+                color = 'rgb(80, 180, 80)'
+                text = f"+{row['diff'].round(1)}%"
+            elif value < 0:
+                color = 'rgb(180, 80, 80)'
+                text = f"{row['diff'].round(1)}%"
+            else:
+                color = 'rgb(220, 220, 220)'
+                text = f"{row['diff'].round(1)}%"
+
+            fig.add_annotation(
+                dict(
+                    xref='x',
+                    yref='y',
+                    x=row[['Sekundarstufe I', 'Sekundarstufe II']].max(),
+                    y=index,
+                    xanchor='left',
+                    text=text,
+                    font=dict(
+                        family='Arial',
+                        size=14,
+                        color=color
+                    ),
+                    showarrow=False
+                )
+            )
 
         return fig
 
+
 def plot_states(topic_model: BERTopic, df: pd.DataFrame):
-
-
     topics = topic_model.get_topic_info()
 
     probabilities = topic_model.probabilities_
@@ -147,15 +210,14 @@ def plot_states(topic_model: BERTopic, df: pd.DataFrame):
 
     prop[OTHER_LABEL] = 1 - prop.sum(axis=1)
 
-    prop[df['bundesland'].isin(complete_states)]
+    prop[df['bundesland'].isin(COMPLETE_STATES)]
 
-    df_grouped = pd.concat([df[['bundesland', 'stufe']], pd.DataFrame(topic_model.probabilities_)], axis=1)\
-
+    df_grouped = pd.concat([df[['bundesland', 'stufe']], pd.DataFrame(topic_model.probabilities_)], axis=1)
     df_agg = df_grouped.groupby(['bundesland', 'stufe']).mean()
 
-    #agg = cur_df.rename(short_labels, axis=1)
+    # agg = cur_df.rename(short_labels, axis=1)
 
-    df_agg = df_agg.loc[(complete_states)]
+    df_agg = df_agg.loc[(COMPLETE_STATES)]
 
     df_agg = df_agg.groupby('bundesland').mean()
 
@@ -169,4 +231,3 @@ def plot_states(topic_model: BERTopic, df: pd.DataFrame):
     )
 
     return fig
-
